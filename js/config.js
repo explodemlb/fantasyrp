38fad1f4c0932ee8687840a3e0d050e91815e512
explodemlb <44819544+explodemlb@users.noreply.github.com>
2021-03-30 12:51:54 -0700

    yes

--- js/config.js
+++ js/config.js
@@ -73,28 +73,6 @@ var l_bgImageMapBased = false;
 // (You can also use external URL)
 var l_bgImages = [
 	"1.jpg",
-	"2.jpg",
-	"3.jpg",
-	"4.jpg",
-	"5.jpg",
-	"6.jpg",
-	"7.jpg",
-	"8.jpg",
-	"9.jpg",
-	"10.jpg",
-	"11.jpg",
-	"12.jpg",
-	"13.jpg",
-	"14.jpg",
-	"15.jpg",
-	"16.jpg",
-	"17.jpg",
-	"18.jpg",
-	"19.jpg",
-	"20.jpg",
-	"21.jpg",
-	"22.jpg",
-	"23.jpg",
 ];
 
 // (Image-background only)
