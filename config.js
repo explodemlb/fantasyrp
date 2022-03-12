f3a5631ecafefa81dc4efa45fdff40d16796008e
explodemlb <44819544+explodemlb@users.noreply.github.com>
2022-03-11 20:13:43 -0800

    Final

--- config.js
+++ config.js
@@ -153,7 +153,7 @@ config.music_random_order = true
 
 // Music volume
 // Choose a value between 0 (quietest) and 100 (loudest)
-config.music_volume = 20
+config.music_volume = 15
 
 // Use Gmod volume for the music instead of manual volume
 config.music_gmod_volume = true
